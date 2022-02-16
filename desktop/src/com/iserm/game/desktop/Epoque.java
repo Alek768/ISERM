798f2ea71b02d15de9ed170cb0c30009d333e237
fluzoy <julien.morese@gmail.com>
2022-02-16 10:38:14 +0100

    Nouvelles classes

---
+++ desktop/src/com/iserm/game/desktop/Epoque.java
@@ -0,0 +1,23 @@
+package com.iserm.game.desktop;
+
+import java.util.ArrayList;
+
+public class Epoque {
+
+    private int date;
+    private String nom;
+    private ArrayList<> listeDesMinerais;
+    private boolean unlock;
+
+
+    /**
+     * Cette méthode est appellée quand un joueur clique sur une époque
+     * @return Elle lui renvoi ainsi les infos necessaire sur l'époque concernée (peut être sous la forme d'une petite fenêtre pop-up?
+     *
+     */
+    public ArrayList<> questionnerLEpoque() {
+        return null;
+    }
+
+
+}
---
+++ desktop/src/com/iserm/game/desktop/Mines.java
@@ -0,0 +1,13 @@
+package com.iserm.game.desktop;
+
+import java.util.ArrayList;
+
+public class Mines {
+
+    String nom ;
+    ArrayList<>listeDeMinerais ;
+    int
+
+
+
+}
