26bb5bb679ef8cf4a5591f74af25d2738eb115ae
fluzoy <julien.morese@gmail.com>
2022-02-16 11:47:00 +0100

    Nouvelles classes pour le diagramme

--- desktop/src/com/iserm/game/desktop/Epoque.java
+++ desktop/src/com/iserm/game/desktop/Epoque.java
@@ -4,20 +4,67 @@ import java.util.ArrayList;
 
 public class Epoque {
 
-    private int date;
+    private int dateDebut;
+    private int dateFin;
     private String nom;
-    private ArrayList<> listeDesMinerais;
+    private ArrayList<Minerais> listeDesMinerais = new ArrayList<>();
+    /**
+     * Permet de définir le minerais dominant de l'époque
+     */
+    private Minerais mineraisDominant;
     private boolean unlock;
+    private Theme theme;
 
+
+    public Epoque(int dateDebut, int dateFin, String nom, ArrayList<Minerais> listeDesMinerais, Minerais mineraisDominant, boolean unlock, int themeMusicale) {
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.nom = nom;
+        this.listeDesMinerais = listeDesMinerais;
+        this.mineraisDominant = mineraisDominant;
+        this.unlock = unlock;
+        this.themeMusicale = themeMusicale;
+    }
 
     /**
      * Cette méthode est appellée quand un joueur clique sur une époque
-     * @return Elle lui renvoi ainsi les infos necessaire sur l'époque concernée (peut être sous la forme d'une petite fenêtre pop-up?
      *
+     * @return Elle lui renvoi ainsi les infos necessaire sur l'époque concernée (peut être sous la forme d'une petite fenêtre pop-up?
      */
-    public ArrayList<> questionnerLEpoque() {
+    public ArrayList<Minerais> questionnerLEpoque() {
         return null;
     }
 
+    public int getDateDebut() {
+        return dateDebut;
+    }
 
+    public int getDateFin() {
+        return dateFin;
+    }
+
+    /**
+     * Liste de getter
+     */
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public ArrayList getListeDesMinerais() {
+        return listeDesMinerais;
+    }
+
+    public Minerais getMineraisDominant() {
+        return mineraisDominant;
+    }
+
+    public boolean isUnlock() {
+        return unlock;
+    }
+
+    public int getThemeMusicale() {
+        return themeMusicale;
+    }
 }
