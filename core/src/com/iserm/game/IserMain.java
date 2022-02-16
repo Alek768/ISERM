a8fc0b3d73372bf0c9b51a7dd1e8110790ea10e8
Alek768 <alek.tamoyan@mines-ales.fr>
2022-02-16 11:48:14 +0100

    Classe IG

--- core/src/com/iserm/game/IserMain.java
+++ core/src/com/iserm/game/IserMain.java
@@ -1,25 +1,42 @@
 package com.iserm.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class IserMain extends ApplicationAdapter {
 	SpriteBatch batch;
+	OrthographicCamera camera;
 	Texture img;
+	Texture img2;
+
+
+
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("ISERM Logo.png");
+		img2 = new Texture("Logo-IMT-Mines-Ales.jpeg");
 	}
 
 	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(false,width,height);
+	}
+
+
+	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 180, 170);
+		batch.draw(img2, 530, 420);
 		batch.end();
 	}
 	
