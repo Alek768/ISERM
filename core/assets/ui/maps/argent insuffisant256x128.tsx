d117274713613a63378831f1a8aac80054168e07
fluzoy <julien.morese@gmail.com>
2022-03-26 21:54:10 +0100

    ca marche zbi

--- core/assets/ui/maps/argent insuffisant256x128.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="argent insuffisant256x128" tilewidth="256" tileheight="128" tilecount="1" columns="1">
- <image source="../../../../../../Desktop/EMA/S6/CEPI/Work/tiledNew/tilesets/HandMade/Arbre mine fouillable/argent insuffisant256x128.png" width="256" height="128"/>
-</tileset>
--- core/assets/ui/maps/infos mines128x256.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="infos mines128x256" tilewidth="128" tileheight="256" tilecount="1" columns="1">
- <image source="../../../../../../Desktop/EMA/S6/CEPI/Work/tiledNew/tilesets/HandMade/Arbre mine exploiter/infos mines128x256.png" width="128" height="256"/>
-</tileset>
