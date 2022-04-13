e3883b77ddf70e9cd80d3d5c679f0428952d7e5d
thoma <thomas.mehu@mines-ales.org>
2022-04-14 00:26:11 +0200

    modif map

---
+++ core/assets/ui/maps/Stockage128x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Stockage128x32" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="Stockage128x32.png" width="128" height="32"/>
+</tileset>
