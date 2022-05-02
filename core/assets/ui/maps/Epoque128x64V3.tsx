9b731de363beadaa75c7ed74b456595861279db2
thoma <thomas.mehu@mines-ales.org>
2022-05-02 22:18:52 +0200

    Push final

---
+++ core/assets/ui/maps/Epoque128x64V3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Epoque128x64V3" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="Epoque128x64V3.png" width="128" height="64"/>
+</tileset>
---
+++ core/assets/ui/maps/texte pseudo128_x32V3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="texte pseudo128_x32V3" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="texte pseudo128_x32V3.png" width="128" height="32"/>
+</tileset>
